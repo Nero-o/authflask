e492dea67da857937cb46e52917aed02cf6e105b
Nero-o <omarmahmoud3611@gmail.com>
2024-02-26 21:49:03 -0300

    feat: adicionado novas configuracoes da idea

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,18 @@ from .model.models import db
 from .routes.auth_routes import auth_bp
 from config import Config
 from flask_jwt_extended import JWTManager
-
+from flask_migrate import Migrate  # Import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     jwt = JWTManager(app)
     db.init_app(app)
-    
+
+    # Initialize Flask-Migrate
+    migrate = Migrate(app, db)
+
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    
+
     return app
