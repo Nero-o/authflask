4cf1f563ddd08822c122be54c7a2209a32431cb4
Nero-o <omarmahmoud3611@gmail.com>
2024-02-26 21:50:34 -0300

    feat: novos campos nos models para definir o banco de dados

--- app/model/models.py
+++ app/model/models.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    class User(db.Model):
+        __tablename__ = 'users'
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(50), unique=True, nullable=False)
+        password = db.Column(db.String(255), nullable=False)
+        email = db.Column(db.String(100), unique=True, nullable=False)
+        cpf = db.Column(db.String(11), unique=True, nullable=True)
+        cnpj = db.Column(db.String(14), unique=True, nullable=True)
+
