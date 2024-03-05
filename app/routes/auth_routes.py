7d2f95c8b43e24a9fe935abbd0557cb165349784
Nero-o <omarmahmoud3611@gmail.com>
2024-03-05 13:48:43 -0300

    wip: corrigido problemas de cors

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
-
 from ..services.auth_services import register_user, login_user
 from flask_cors import CORS
+from flask_cors import cross_origin
+
 
 auth_bp = Blueprint('auth', __name__)
 CORS(auth_bp)
@@ -11,6 +12,8 @@ def register():
     return register_user(data)
 
 @auth_bp.route('/login', methods=['POST'])
+@cross_origin(origin='http://localhost:3000/authentication/signin',headers=['Content- Type','Authorization'])
+
 def login():
     data = request.get_json()
     return login_user(data)
